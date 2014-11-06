17286593e76edbf51ca362f3d775cbc30848c28b
Doga <dgulhan@cern.ch>
2014-11-06 22:37:48 +0100

    centrality dependent correction derived with PYTHIA+HYDJET and PF cand pt after Vs subtraction

--- fragmentation_JEC.h
+++ fragmentation_JEC.h
@@ -9,26 +9,37 @@
 struct fragmentation_JEC
 {
  private:
-  TH2D* correction_matrix;
+  static const int ncent=4;
+  TH2D* correction_matrix[ncent];
   TFile* correction_file;
   int radius;
   double PF_pt_cut;
   TString algo_corr; 
   int ntrkmax;
   double PF_eta_cut;
+  bool doPH;
+  int cent_max[ncent];
+  int cent_min[ncent];
  public:
   void reset()
   { 
-   correction_matrix=NULL;
+   for(int icent=0;icent<ncent;icent++){
+    correction_matrix[icent]=NULL;
+   }
    correction_file=NULL;
    ntrkmax=21;
    PF_eta_cut=2.4;
+   cent_min[0]=0;
+   cent_max[0]=cent_min[1]=20;
+   cent_max[1]=cent_min[2]=60;
+   cent_max[2]=cent_min[3]=100;
+   cent_max[3]=200;
   }
   
-  fragmentation_JEC(int radius, double PF_pt_cut)
+  fragmentation_JEC(int radius, double PF_pt_cut=2,bool doPH=1)
   {
    reset();
-   
+   this->doPH=doPH;
    this->radius=radius;
    this->PF_pt_cut=PF_pt_cut;
    if(PF_pt_cut==3) ntrkmax=26;
@@ -36,12 +47,19 @@ struct fragmentation_JEC
    else if(PF_pt_cut==1) ntrkmax=41;
   }
   
-  double get_corrected_pt(double jetpt, int ntrk)
+  double get_corrected_pt(double jetpt, int ntrk, int cent=0)
   {
    double correction=1;
    
-   if(ntrk<ntrkmax) correction=correction_matrix->GetBinContent(correction_matrix->GetXaxis()->FindBin(ntrk),correction_matrix->GetYaxis()->FindBin(jetpt));
-   else correction=correction_matrix->GetBinContent(correction_matrix->GetXaxis()->FindBin(ntrkmax),correction_matrix->GetYaxis()->FindBin(jetpt));
+   int cent_bin=0;
+   if(doPH){
+    for(int icent=0;icent<ncent;icent++){
+     if(cent<cent_max[icent] && cent>=cent_min[icent]) cent_bin=icent;
+    }
+   }
+   
+   if(ntrk<ntrkmax) correction=correction_matrix[cent_bin]->GetBinContent(correction_matrix[cent_bin]->GetXaxis()->FindBin(ntrk),correction_matrix[cent_bin]->GetYaxis()->FindBin(jetpt));
+   else correction=correction_matrix[cent_bin]->GetBinContent(correction_matrix[cent_bin]->GetXaxis()->FindBin(ntrkmax),correction_matrix[cent_bin]->GetYaxis()->FindBin(jetpt));
    
    return (1/correction)*jetpt;
   }
@@ -58,8 +76,17 @@ struct fragmentation_JEC
   
   void set_correction()
   {
-    algo_corr=Form("ak%dCalo",radius);
-    correction_file = new TFile(Form("corrections_id1/FFJEC_correction_PF_%s_pt%d.root",algo_corr.Data(),(int)PF_pt_cut));
-    correction_matrix=(TH2D*)correction_file->Get("pNtrk_pt");
+   cout<<"setting correction"<<endl;
+    if(doPH){
+     algo_corr=Form("akVs%dCalo",radius);
+     correction_file = new TFile(Form("corrections_id1_ph/FFJEC_correction_PF_%s_pt%d.root",algo_corr.Data(),(int)PF_pt_cut));
+     for(int icent=0;icent<ncent;icent++){
+	    correction_matrix[icent]=(TH2D*)correction_file->Get(Form("pNtrk_pt%d",icent));
+	   } 
+	  }else{
+     algo_corr=Form("ak%dCalo",radius);
+     correction_file = new TFile(Form("corrections_id1/FFJEC_correction_PF_%s_pt%d.root",algo_corr.Data(),(int)PF_pt_cut));
+     correction_matrix[0]=(TH2D*)correction_file->Get("pNtrk_pt");
+	 }
   }
 };
